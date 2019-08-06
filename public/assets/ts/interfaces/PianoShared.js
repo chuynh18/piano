620c786f22e891dc4be4023279f8c4b95bbffc1c
chuynh18 <38088640+chuynh18@users.noreply.github.com>
2019-08-06 15:27:09 -0700

    removing transpiled interface files from ts directory

--- public/assets/ts/interfaces/PianoShared.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Damper;
-(function (Damper) {
-    Damper[Damper["None"] = 0] = "None";
-    Damper[Damper["Half"] = 1] = "Half";
-    Damper[Damper["Full"] = 2] = "Full";
-})(Damper = exports.Damper || (exports.Damper = {}));

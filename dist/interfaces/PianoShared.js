a45629406f11f3a39b79fdaa52468ce31a35226c
chuynh18 <38088640+chuynh18@users.noreply.github.com>
2019-08-08 20:51:15 -0700

    removing dist directory

--- dist/interfaces/PianoShared.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Damper;
-(function (Damper) {
-    Damper[Damper["None"] = 0] = "None";
-    Damper[Damper["Half"] = 1] = "Half";
-    Damper[Damper["Full"] = 2] = "Full";
-})(Damper = exports.Damper || (exports.Damper = {}));
-//# sourceMappingURL=PianoShared.js.map
--- dist/main.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Player_1 = require("./Player");
-var player;
-window.addEventListener("load", function () {
-    player = new Player_1.Player("kb");
-});
-//# sourceMappingURL=main.js.map
